4d83ecbb008ef966ef615249d5b2e82c1ad040ec
CostaCesar <caioplays@outlook.com>
2024-09-12 17:30:59 -0300

    ADD: [Vector] Definitions

---
+++ Structures/Vector/vector.h
@@ -0,0 +1,38 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <stdint.h>
+
+typedef struct
+{
+   void* data;
+   size_t size;
+} Vector;
+
+Vector Vector_Create();
+Vector Vector_CreateEmpty(size_t vector_size);
+uint32_t Vector_IsEmpty(const Vector* Vector);
+void Vector_Free(Vector* free_vector);
+
+void Vector_Push(Vector* insert_vector, size_t index, void* data);
+void Vector_PushFront(Vector* insert_vector, void* data);
+void Vector_PushBack(Vector* insert_vector, void* data);
+
+void* Vector_Peek(Vector* peek_vector, size_t index);
+void* Vector_PeekFront(Vector* peek_vector);
+void* Vector_PeekBack(Vector* peek_vector);
+
+uint32_t Vector_CheckData(const Vector* search_vector, const void* search_key,
+    uint32_t (*comparator_function)(const void*,const void*));
+uint32_t Vector_GetIndex(const Vector* search_vector, const void* search_key,
+    uint32_t (*comparator_function)(const void*,const void*));
+
+void Vector_Pop(Vector* pop_vector, size_t index);
+void Vector_PopFront(Vector* spop_vector);
+void Vector_PopBack(Vector* spop_vector);
+
+void Vector_Join(Vector* destiny_vector, const Vector* source_vector, uint32_t index);
+void Vector_Append(Vector* destiny_vector, const Vector* source_vector);
+void Vector_Preappend(Vector* destiny_vector, const Vector* source_vector);
+
+#endif
