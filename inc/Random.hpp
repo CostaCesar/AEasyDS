61688c46b3ece8868e76a7ce16b1a2eccdccfad1
CostaCesar <caioplays@outlook.com>
2025-09-10 23:14:46 -0300

    ADD: [PRNG] 64 bit LFSR

--- inc/Random.hpp
+++ inc/Random.hpp
@@ -19,4 +19,22 @@ public:
     void Reset();
     uint64_t Rand();
 };
+
+class LFSR_64
+{
+private:
+    uint64_t state;
+    uint64_t seed;
+
+public:
+    LFSR_64(uint64_t _seed = 1);
+
+    void Seed(uint64_t new_seed);
+    uint64_t Seed();
+    void Reset();
+    uint64_t Rand();
+};
+
+} // namespace Aeasyds :: Random
+
 #endif
--- src/Random.cpp
+++ src/Random.cpp
@@ -32,4 +32,33 @@ uint64_t LCG_64::Rand()
     state = (MULT * state + ADDC);
     return state;
 }
+
+/* LFSR_64 implementation */
+
+LFSR_64::LFSR_64(uint64_t _seed)
+{
+    this->seed = _seed;
+    this->state = this->seed;
+}
+void LFSR_64::Seed(uint64_t new_seed)
+{
+    this->seed = new_seed;
+    Reset();
+}
+uint64_t LFSR_64::Seed()
+{
+    return seed;
+}
+void LFSR_64::Reset()
+{
+    this->state = seed;
+}
+uint64_t LFSR_64::Rand()
+{
+    state ^= (state << 21);
+    state ^= (state >> 30);
+    state ^= (state << 4);
+    return state;
+}
+
 } // namespace Aeasyds::Random
