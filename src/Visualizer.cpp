57567f383bf79ac867b4a97541709de4c9e8bf1e
CostaCesar <caioplays@outlook.com>
2025-09-10 23:15:13 -0300

    ADD: PRNG Use in Viewer

--- src/Visualizer.cpp
+++ src/Visualizer.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 #include "structs/PQueue.hpp"
+#include "Random.hpp"
 
 int main(int argc, char const *argv[])
 {
-    srand(123456789);
     
     const int k_size = 50;
     auto GetPriority = [](const int& num){
@@ -16,10 +16,26 @@ int main(int argc, char const *argv[])
         b = x;
     };
     Aeasyds::PQueue<int> queue(k_size, GetPriority, Swap);
-
+    
+    Aeasyds::Random::LFSR_64 generator(1234567890);
     for (int i = 0; i < k_size / 2; i++)
     {
-        int a = rand() % 10;
+        int a = (generator.Rand() % 0xffffffff) % 10;
+        queue.Insert(a);
+        std::cout << "Inserted " << a << std::endl;
+    }
+    while (queue.Size() > 5)
+    {
+        int b = queue.Remove();
+        std::cout << "Removed " << b << std::endl;
+    }
+    std::cout << "Current size: " << queue.Size() << std::endl;
+    std::cout << "Next on queue: " << queue.Get() << std::endl;
+
+    generator.Seed(987654321);
+    for (int i = 0; i < k_size / 2; i++)
+    {
+        int a = (generator.Rand() % 0xffffffff) % 10;
         queue.Insert(a);
         std::cout << "Inserted " << a << std::endl;
     }
